12dbf0e823a495e3f40d9194bda8bbdb0e6a6ee2
paulwstorm <58491028+paulwstorm@users.noreply.github.com>
2021-10-25 19:13:50 -0700

    Adding additional files and data loading

---
+++ codingchallenge/src/test/java/com/thecodacollection/codingchallenge/CodingchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodacollection.codingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
